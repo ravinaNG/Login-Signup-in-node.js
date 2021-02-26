fcaa2e3dd04ca6988c61b1b428cbea16002930ae
Ravina Kumari <ravina18@navgurukul.org>
2021-02-26 19:49:09 +0530

    I have started making the project.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const { rejects } = require('assert')
+const fs = require('fs')
+const { resolve } = require('path')
 const readline = require('readline-sync')
-const user = readline.question("Login/Sign-up:- ")
-console.log(user);
+
+const user = new Promise((resolve, reject) => {
+    let responce = readline.question("Login(L or l) / Sign-up(S or s):- ");
+    if(responce == "S" || responce == "s"){
+        resolve("signUp");
+    }else{
+        reject("logIn");
+    }
+});
+
+const signUp = new Promise((resolve, reject) => {
+    if(user == "S" || user == "s"){
+        let userName = readline.question("What will be you userName:- ");
+        let passwrd1 = readline.question("Security code for you ID:- ");
+        let passwrd2 = readline.question("Re enter your password:- ");
+        resolve()
+    }
+})
+
