73583735d3095b836c97b0be38053581838052a1
Ravina Kumari <ravina18@navgurukul.org>
2021-02-26 22:45:54 +0530

    converted the code into promises using code

--- main.js
+++ main.js
@@ -1,74 +1,61 @@
-const fs = require('fs')
+const fs = require('fs');
+const { resolve } = require('path');
 const readline = require('readline-sync')
 
 function paswrdValidation(password){
-    var checking = 0;
-    if(password.includes("@") || password.includes("#")){
-        var num = 0
-        while(num<10){
-            if(password.includes(num)){
-                checking = 1
-                break;
+    return responce = new Promise((resolve, reject) => {
+        var checking = 0;
+        if(password.includes("@") || password.includes("#")){
+            var num = 0
+            while(num<10){
+                if(password.includes(num)){
+                    checking = 1
+                    break;
+                }
+                num +=1;
             }
-            num +=1;
-        }
-        if(checking === 0){
-            return false; 
+            if(checking === 0){
+                reject(false); 
+            }else{
+                resolve(true);
+            }
         }else{
-            return true;
+            reject(false)
         }
-    }else{
-
-        return false;
-    }
+    })
 }
 
+function writeJsonFile(fileName, data){
+    return responce = new Promise((resolve, reject) => {
+        let userData = JSON.stringify(data);
+        fs.writeFileSync(fileName, userData); //'userDetails.json'
+        resolve(`congrats ${data["user"][0]["userName"]} you are Signed Up Successfully`)
+    })
+}
 
 
 let user = readline.question("Login(L or l) / Sign-up(S or s):- ");
 
 if(user === "S" || user === "s"){
-    const userName = readline.question("What will be you userName:- ");
-    const passwrd1 = readline.question("Security code for you ID:- ");
+    const userName = readline.question("What will be your userName:- ");
+    const passwrd1 = readline.question("Security code for your ID:- ");
     const passwrd2 = readline.question("Re enter your password:- ");
     if(passwrd1 === passwrd2){
-        if(paswrdValidation(passwrd1)){
+        paswrdValidation(passwrd1).then(()=>{
             let userDetails = {
                 "user":[{
                     "userName": userName,
                     "password": passwrd1
                 }]
             }
-            let data = JSON.stringify(userDetails);
-            fs.writeFileSync('userDetails.json', data);
-        }else{
-            console.log("At least password should contain one special character and one number.");
-        }
+            
+            let fileName = "userDetails.json"
+            return writeJsonFile(fileName, userDetails)
+        }).then((message) => {
+            console.log(message);
+        })
     }else{
         console.log("Both password are not same.");
     }
 };
-
-const loginSignup = () => {
-    
-}
-
-
-// const user = new Promise((resolve, reject) => {
-//     let responce = readline.question("Login(L or l) / Sign-up(S or s):- ");
-//     if(responce == "S" || responce == "s"){
-//         resolve("signUp");
-//     }else{
-//         reject("logIn");
-//     }
-// });
-
-// const signUp = new Promise((resolve, reject) => {
-//     if(user == "S" || user == "s"){
-//         let userName = readline.question("What will be you userName:- ");
-//         let passwrd1 = readline.question("Security code for you ID:- ");
-//         let passwrd2 = readline.question("Re enter your password:- ");
-//         resolve()
-//     }
-// })
 
