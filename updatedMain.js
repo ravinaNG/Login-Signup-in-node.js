f960a97d0695d99de675732803526607179d06c3
Ravina Kumari <ravina18@navgurukul.org>
2021-03-02 11:50:08 +0530

    while login I have to check if the password is same

--- updatedMain.js
+++ updatedMain.js
@@ -58,6 +58,21 @@ function isExist(userName, jsonData){
     return false;
 }
 
+function isThePaswrdSame(userName, password, readfileData){
+    let index = 0;
+    while(index < readfileData['user'].length){
+        if(userName === readfileData['user'][index]['userName']){
+            break
+        }
+        index +=1
+    }
+    if(password === readfileData['user'][index]['password']){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 let user = readline.question("Login(L or l) / Sign-up(S or s):- ");
 
 if(user === "S" || user === "s"){
@@ -140,68 +155,6 @@ if(user === "S" || user === "s"){
             }
         }
     })
-    // const passwrd1 = readline.question("Security code for your ID:- ");
-    // const passwrd2 = readline.question("Re enter your password:- ");
-    // if(passwrd1 === passwrd2){
-    //     paswrdValidation(passwrd1).then(()=>{
-    //         let fileName = "userDetails.json"
-    //         let jsonFileData = readJsonFile(fileName); 
-    //         return jsonFileData;
-    //     }).then((userDetails) => {
-    //         let oneUserDetails = {
-    //             "user":[{
-    //                 "userName": userName,
-    //                 "password": passwrd1
-    //             }]
-    //         }
-    //         if(userDetails === 1){
-    //             fileName = "userDetails.json"
-    //             console.log("");
-    //             console.log("******");
-    //             console.log(`Congrats ${userName} you are signed up successfully.`);
-    //             console.log("******");
-    //             console.log("");
-    //             const description = readline.question("Please describe yourself:- ");
-    //             const dob = readline.question("Date of Birth:- ");
-    //             const hobbies = readline.question("Your hobbies:- ");
-    //             const gender = readline.question("Enter you Gender:- ");
-    //             oneUserDetails['user'][0]["profile"] = {
-    //                 "description": description,
-    //                 "dob": dob,
-    //                 "hobbies": hobbies,
-    //                 "gender": gender
-    //             }
-    //             writeJsonFile(fileName, oneUserDetails)
-    //         }else{
-    //             let exist = isExist(userName, userDetails);
-    //             if(exist){
-    //                 console.log("Username already exist.");
-    //             }else{
-    //                 fileName = "userDetails.json";
-    //                 oneUserData = oneUserDetails["user"][0];
-    //                 console.log("");
-    //                 console.log("******");
-    //                 console.log(`Congrats ${userName} you are signed up successfully.`);
-    //                 console.log("******");
-    //                 console.log("");
-    //                 const description = readline.question("Please describe yourself:- ");
-    //                 const dob = readline.question("Date of Birth:- ");
-    //                 const hobbies = readline.question("Your hobbies:- ");
-    //                 const gender = readline.question("Enter you Gender:- ");
-    //                 oneUserData["profile"] = {
-    //                     "description": description,
-    //                     "dob": dob,
-    //                     "hobbies": hobbies,
-    //                     "gender": gender
-    //                 }
-    //                 userDetails["user"].push(oneUserData);
-    //                 writeJsonFile(fileName, userDetails)
-    //             }
-    //         }
-    //     })
-    // }else{
-    //     console.log("Both password are not same.");
-    // }
 }else if(user === "L" || user === "l"){
     const userName = readline.question("Please enter your userName:- ");
     const passwrd = readline.question("Password:- ");
@@ -209,21 +162,26 @@ if(user === "S" || user === "s"){
     readJsonFile(fileName).then((userDetails) =>{
         let exist = isExist(userName, userDetails);
         if(exist){
-            console.log("");
-            console.log("******");
-            console.log(`${userName} you are logged in successfully.`);
-            console.log("******");
-            console.log("");
-            let index = 0;
-            while(index < userDetails['user'].length){
-                if(userName === userDetails['user'][index]['userName']){
-                    console.log(`Username: ${userName}`);
-                    console.log(`Gender: ${userDetails['user'][index]['profile']['gender']}`);
-                    console.log(`Bio: ${userDetails['user'][index]['profile']['description']}`);
-                    console.log(`Hobbies: ${userDetails['user'][index]['profile']['hobbies']}`);
-                    console.log(`Dob: ${userDetails['user'][index]['profile']['dob']}`);
+            let matchingPaswrd = isThePaswrdSame(userName, passwrd, userDetails)
+            if(matchingPaswrd){
+                console.log("");
+                console.log("******");
+                console.log(`${userName} you are logged in successfully.`);
+                console.log("******");
+                console.log("");
+                let index = 0;
+                while(index < userDetails['user'].length){
+                    if(userName === userDetails['user'][index]['userName']){
+                        console.log(`Username: ${userName}`);
+                        console.log(`Gender: ${userDetails['user'][index]['profile']['gender']}`);
+                        console.log(`Bio: ${userDetails['user'][index]['profile']['description']}`);
+                        console.log(`Hobbies: ${userDetails['user'][index]['profile']['hobbies']}`);
+                        console.log(`Dob: ${userDetails['user'][index]['profile']['dob']}`);
+                    }
+                    index +=1
                 }
-                index +=1
+            }else{
+                console.log("Wrong Password");
             }
         }else{
             console.log("Invalid userName and password.");
